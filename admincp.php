31225f2b4cc4cb73a70b217ebe3c434fc2237ff3
tutnhtecom <tu.tran@htecomm.vn>
2025-04-08 10:40:02 +0700

    Fix

--- admincp.php
+++ admincp.php
@@ -1,11 +1,10 @@
 <?php
 
 require 'assets/init.php';
-
 $is_admin = Wo_IsAdmin();
 $is_moderoter = Wo_IsModerator();
 // Get header
-include('components/admincp/admincp_router.php');
+include('components/admincp/admincp_header.php');
 // Get request
 include('components/admincp/admincp_request.php');
 
